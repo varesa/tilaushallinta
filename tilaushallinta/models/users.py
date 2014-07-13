86913701c8443675e32bf57dc123f36973e17aa6
Esa Varemo <esa@kuivanto.fi>
2014-07-13 22:45:40 +0300

    Added a newline

--- tilaushallinta/models/users.py
+++ tilaushallinta/models/users.py
@@ -2,6 +2,7 @@
 # This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 # Author: Esa Varemo
 #
+
 import bcrypt
 
 from sqlalchemy import Column, Text, Integer, DateTime, Boolean
