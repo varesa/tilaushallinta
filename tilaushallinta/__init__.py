0cc9598d3ed68b885a3e893fc035a211e4c8c563
Esa Varemo <esa@kuivanto.fi>
2014-07-14 15:22:41 +0300

    Giving pages better titles than '...'

--- tilaushallinta/__init__.py
+++ tilaushallinta/__init__.py
@@ -4,6 +4,9 @@
 #
 
 from pyramid.config import Configurator
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+
 from sqlalchemy import engine_from_config
 
 from .models import (
@@ -20,8 +23,14 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+
+    authn_policy = AuthTktAuthenticationPolicy('g&hjK/(%SS', hashalg='sha512')
+    authz_policy = ACLAuthorizationPolicy()
+
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
 
     config.add_static_view('static', 'static', cache_max_age=3600)
 
