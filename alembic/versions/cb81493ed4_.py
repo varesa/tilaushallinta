95501dc02f3bf562f1801fd152d4944ec8ea67bf
Esa Varemo <esa@kuivanto.fi>
2014-08-11 02:37:04 +0300

    Adding current alembic versions

---
+++ alembic/versions/cb81493ed4_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: cb81493ed4
+Revises: None
+Create Date: 2014-08-09 18:53:43.392798
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'cb81493ed4'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
