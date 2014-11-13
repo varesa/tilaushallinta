0609350ceb67c731afd4490f167129eed8818bae
Esa Varemo <esa@kuivanto.fi>
2014-11-13 19:18:26 +0200

    Allow selection of old order information (but not it's usage yet)

--- tilaushallinta/static/js/neworder_history_modal.js
+++ tilaushallinta/static/js/neworder_history_modal.js
@@ -4,7 +4,41 @@
  * Copyright Esa Varemo 2014
  */
 
-function show_history(type) {
+var selection_type = "";
 
+function show_history(type) {
+    $('.row_data').remove();
+
+    selection_type = type;
+
+    var url = "";
+    if(type == "tilaaja") {
+        url = '/json/tilaajat_newest'
+    } else {
+        url = '/json/kohteet_newest'
+    }
+    $.get(url, function(data) {
+        console.log(type);
+        for(var i = 0; i < data.length; i++) {
+            var namerow = "";
+            if(data[i]['yritys'].length > 0) {
+                namerow += data[i]['yritys'];
+                if(data[i]['nimi'].length > 0) {
+                    namerow += ", " + data[i]['nimi'];
+                }
+            } else {
+                namerow = data[i]['nimi'];
+            }
+
+            var addressrow = data[i]['osoite'] + ", " + data[i]['postinumero'] + ' ' + data[i]['postitoimipaikka'];
+
+            $('#table_history').append("<tr class=\"row_data\">" +
+                "<td><input type=\"radio\"></td>" +
+                "<td>" + namerow + "</td>" +
+                "<td>" + addressrow +  "</td>" +
+                "</tr>")
+        }
+    });
     $('.modal').modal(/*options?*/);
+
 }
