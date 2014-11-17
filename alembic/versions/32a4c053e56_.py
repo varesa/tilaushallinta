73368c3f9d8ffac387360741a027695c3b10f413
Esa Varemo <esa@kuivanto.fi>
2014-11-17 03:35:30 +0200

    Add a new "initial" alembic revision

---
+++ alembic/versions/32a4c053e56_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 32a4c053e56
+Revises: None
+Create Date: 2014-11-17 03:34:16.287483
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '32a4c053e56'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
