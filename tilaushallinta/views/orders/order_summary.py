6972bf5f88de7ee004cc2fd50d324e0db91b7189
Esa Varemo <esa@kuivanto.fi>
2015-01-03 03:54:21 +0200

    Separated python code from the commit, visual fixes

--- tilaushallinta/views/orders/order_summary.py
+++ tilaushallinta/views/orders/order_summary.py
@@ -14,4 +14,29 @@ from ..common import DBSession
 def view_order_summary(request):
     tilaus_id = request.matchdict['id']
     tilaus = DBSession.query(Tilaus).filter_by(id=tilaus_id).first()
-    return {'tilaus': tilaus}
+
+    luokka1 = False
+    luokka2 = False
+    luokka3 = False
+    for raportti in tilaus.paivaraportit:
+            if raportti.hintaluokka == 1:
+                    if not luokka1:
+                            luokka1 = {'tunnit': 0.0, 'matkat': 0.0, 'muut': 0.0}
+                    luokka1['tunnit'] += raportti.tunnit
+                    luokka1['matkat'] += raportti.matkat
+                    luokka1['muut']   += raportti.muut
+            if raportti.hintaluokka == 2:
+                    if not luokka2:
+                            luokka2 = {'tunnit': 0.0, 'matkat': 0.0, 'muut': 0.0}
+                    luokka2['tunnit'] += raportti.tunnit
+                    luokka2['matkat'] += raportti.matkat
+                    luokka2['muut']   += raportti.muut
+            if raportti.hintaluokka == 3:
+                    if not luokka3:
+                            luokka3 = {'tunnit': 0.0, 'matkat': 0.0, 'muut': 0.0}
+                    luokka3['tunnit'] += raportti.tunnit
+                    luokka3['matkat'] += raportti.matkat
+                    luokka3['muut']   += raportti.muut
+
+    return {'tilaus': tilaus, 'luokka1': luokka1, 'luokka2': luokka2, 'luokka3': luokka3}
+
