4e9dc88c52fbb62b0a8f2100aa06a681a2b92651
Esa Varemo <esa@kuivanto.fi>
2015-08-04 13:47:14 +0300

    huoltosopimukset part 2

--- tilaushallinta/views/huoltosopimukset/huoltosopimus_new.py
+++ tilaushallinta/views/huoltosopimukset/huoltosopimus_new.py
@@ -16,7 +16,7 @@ from tilaushallinta.models import Huoltosopimus
 from tilaushallinta.views.misc.tilaaja_kohde import get_tilaaja_from_r, get_kohde_from_r
 
 
-@view_config(route_name='huoltosopimus_new', renderer='../../templates/huoltosopimukset/order_new.pt')
+@view_config(route_name='huoltosopimus_new', renderer='../../templates/huoltosopimus/huoltosopimus_new.pt')
 def view_huoltosopimus_new(request):
     """
     View method that shows the ordering form
