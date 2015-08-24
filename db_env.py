ab9c0b6bf4a8e3d136587bdd9cbdea40da74467f
Esa Varemo <esa@kuivanto.fi>
2015-08-24 13:27:11 +0300

    Reduce db-var code repetition, update .gitignore

--- db_env.py
+++ db_env.py
@@ -9,37 +9,28 @@ var_pass = "DBPASS"
 file_pass = "dbpassword"
 
 
-def get_host():
-    if var_host in os.environ.keys() and len(os.environ[var_host]) > 0:
-        return os.environ[var_host].strip()
-    elif os.path.isfile(file_host):
-        with open(file_host) as host:
-            return host.readline().strip()
+def _get_value(var_name, file_name):
+    if var_name in os.environ.keys() and len(os.environ[var_name]) > 0:
+        return os.environ[var_name].strip()
+    elif os.path.isfile(file_name):
+        with open(file_name) as f:
+            return f.readline().strip()
     else:
-        print("Neither ENV['" + var_host + "'] or file " + file_host + " provided")
+        print("Neither ENV['" + var_name + "'] or file " + file_name + " provided")
         sys.exit(1)
+
+
+def get_host():
+    return _get_value(var_host, file_host)
 
 
 def get_user():
-    if var_user in os.environ.keys() and len(os.environ[var_user]) > 0:
-        return os.environ[var_user].strip()
-    elif os.path.isfile(file_user):
-        with open(file_user) as user:
-            return user.readline().strip()
-    else:
-        print("Neither ENV['" + var_user + "'] or file " + file_user + " provided")
-        sys.exit(1)
+    return _get_value(var_user, file_user)
 
 
 def get_pass():
-    if var_pass in os.environ.keys() and len(os.environ[var_pass]) > 0:
-        return os.environ[var_pass].strip()
-    elif os.path.isfile(file_pass):
-        with open(file_pass) as passwd:
-            return passwd.readline().strip()
-    else:
-        print("Neither ENV['" + var_pass + "'] or file " + file_pass + " provided")
-        sys.exit(1)
+    return _get_value(var_pass, file_pass)
+
 
 def substitute(url):
     return url\
