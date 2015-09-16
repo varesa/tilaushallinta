77a6bd3314b76d550b88a1abd8443b901457eae4
Esa Varemo <esa@kuivanto.fi>
2015-09-16 20:13:49 +0300

    Korjaustarve checkbox

--- tilaushallinta/views/huoltosopimukset/huolto_details_raportit.py
+++ tilaushallinta/views/huoltosopimukset/huolto_details_raportit.py
@@ -8,8 +8,6 @@
 import datetime
 from tilaushallinta.models import Huoltoraportti
 from tilaushallinta import DBSession
-from tilaushallinta.models.huolto import Lisatoimenpide
-from tilaushallinta.views.utils import string_to_int_or_zero, string_to_float_or_zero
 
 
 def add_huoltoraportti(huolto):
@@ -19,15 +17,6 @@ def add_huoltoraportti(huolto):
     :rtype: None
     """
     huolto.huoltoraportit.append(Huoltoraportti(date=datetime.datetime.now()))
-
-
-def add_lisatoimenpide(huolto):
-    """
-    Create a new Lisatoimenpide in the database
-    :param huolto: The order in which to create the new report
-    :rtype: None
-    """
-    huolto.lisatoimenpiteet.append(Lisatoimenpide(date=datetime.datetime.now(), hintaluokka=2))
 
 
 def form_to_dict(request):
@@ -64,22 +53,11 @@ def raportti_modify_from_dict(raportti_id, raportti_dict):
     raportti = DBSession.query(Huoltoraportti).filter_by(id=raportti_id).first()
     raportti.teksti = raportti_dict['teksti']
 
-
-def lisatoimenpide_modify_from_dict(toimenpide_id, toimenpide_dict):
-    """
-    Modify a Lisatoimenpide object with data from a dictionary
-    :param toimenpide_id: id of the object to modify
-    :type toimenpide_id: int
-    :param toimenpide_dict: dictionary with the new data
-    :type toimenpide_dict: dict
-    :rtype: None
-    """
-    toimenpide = DBSession.query(Lisatoimenpide).filter_by(id=toimenpide_id).first()
-    toimenpide.teksti = toimenpide_dict['teksti']
-    toimenpide.hintaluokka = string_to_int_or_zero(toimenpide_dict['hintaluokka'])
-    toimenpide.matkat = string_to_float_or_zero(toimenpide_dict['matkat'])
-    toimenpide.tunnit = string_to_float_or_zero(toimenpide_dict['tunnit'])
-    toimenpide.muut = string_to_float_or_zero(toimenpide_dict['muut'])
+    if 'kt' in raportti_dict:
+        kt = True
+    else:
+        kt = False
+    raportti.korjaustarve = kt
 
 
 def save_huoltoraportit(request, huolto):
@@ -97,20 +75,3 @@ def save_huoltoraportit(request, huolto):
         for raportti_old in huolto.huoltoraportit:
             if int(raportti_old.id) == int(raportti_id):
                 raportti_modify_from_dict(raportti_id, raportti_new)
-
-
-def save_lisatoimenpiteet(request, huolto):
-    """
-    Save the Lisatoimenpide data
-    :param request: Request from client containing new values as POST data
-    :type request: Request
-    :param huolto: Huolto object to save the data in
-    :type huolto: Huolto
-    :rtype: None
-    """
-    raportit_request = form_to_dict(request)
-
-    for raportti_id, raportti_new in raportit_request.items():
-        for raportti_old in huolto.huoltoraportit:
-            if int(raportti_old.id) == int(raportti_id):
-                lisatoimenpide_modify_from_dict(raportti_id, raportti_new)
