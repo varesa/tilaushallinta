2e1aab9b4fa578d43f4c0932d713191c01f38f9b
Esa Varemo <esa@kuivanto.fi>
2016-02-04 16:52:29 +0200

    Copyright header years - #LSV-246 Fixed

--- tilaushallinta/static/js/order_state.js
+++ tilaushallinta/static/js/order_state.js
@@ -1,7 +1,7 @@
 /*
  * This source code is licensed under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
- * Copyright Esa Varemo 2014-2015
+ * Copyright Esa Varemo 2014-2016
  */
 
 var tilaus = {
