af3ed95fdc9bcd483845d8c054536d16cc3b52cb
Esa Varemo <esa@kuivanto.fi>
2017-09-16 20:40:15 +0300

    Remove unused code

--- tilaushallinta/views/huoltosopimukset/huoltosopimus_huollot.py
+++ tilaushallinta/views/huoltosopimukset/huoltosopimus_huollot.py
@@ -13,7 +13,6 @@ from pyramid.view import view_config
 from tilaushallinta.models import DBSession
 from tilaushallinta.models import Huolto
 from tilaushallinta.models.huoltosopimus import Huoltosopimus
-from tilaushallinta.views.shared.utils import get_closest_date
 
 STATUS_NONE = 0
 STATUS_CLOSE = 1
