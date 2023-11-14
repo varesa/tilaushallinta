e21b62400a128daca99b7161419cd8f187fdf7ba
Esa Varemo <git@esav.fi>
2023-11-14 17:30:07 +0200

    Allow overriding db port

--- db_env.py
+++ db_env.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 var_host = "DBHOST"
-file_host= "config/dbhost"
+file_host = "config/dbhost"
+
+var_port = "PORT"
+file_port = "config/dbport"
 
 var_user = "DBUSER"
 file_user = "config/dbuser"
@@ -14,19 +17,25 @@ var_schema = "DBSCHEMA"
 file_schema = "config/dbschema"
 
 
-def _get_value(var_name, file_name):
+def _get_value(var_name, file_name, default=None):
     if var_name in os.environ.keys() and len(os.environ[var_name]) > 0:
         return os.environ[var_name].strip()
     elif os.path.isfile(file_name):
         with open(file_name) as f:
             return f.readline().strip()
+    elif default:
+        return default
     else:
         print("Neither ENV['" + var_name + "'] or file " + file_name + " provided")
         sys.exit(1)
 
 
 def get_host():
-    return _get_value(var_host, file_host)
+    return _get_value(var_host, file_host, '127.0.0.1')
+
+
+def get_port():
+    return _get_value(var_port, file_port, '3306')
 
 
 def get_user():
@@ -42,8 +51,11 @@ def get_schema():
 
 
 def substitute(url):
-    return url\
-        .replace('<host>', get_host())\
-        .replace('<user>', get_user())\
-        .replace('<password>', get_pass())\
+    return (
+        url
+        .replace('<host>', get_host())
+        .replace('<port>', get_port())
+        .replace('<user>', get_user())
+        .replace('<password>', get_pass())
         .replace('<schema>', get_schema())
+    )
