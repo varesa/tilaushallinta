550e079328b4f9c8c0d16e9d6880894f239a9c13
Esa Varemo <git@esav.fi>
2023-11-14 18:20:02 +0200

    Add anonymization behind a feature toggle

--- tilaushallinta/views/admin/admin_anonymize.py
+++ tilaushallinta/views/admin/admin_anonymize.py
@@ -51,18 +51,20 @@ def view_admin_anonymize(request):
     Replace all sensitive customer data in the database with generic sample data. Useful for converting production data
     to publicly demo-able. Commented out by default as a safety
     """
-    """arr = []
-    with open("tilaushallinta/views/admin/anon.csv", "r") as data:
-        for line in data.readlines():
-            arr.append(line.split('|'))
 
-    for tilaaja in DBSession.query(Tilaaja).all():
-        data = random.choice(arr)
-        anonymize_entity(tilaaja, data)
+    if os.environ['DANGEROUS_FEATURES'] == "enable":
+        arr = []
+        with open("tilaushallinta/views/admin/anon.csv", "r") as data:
+            for line in data.readlines():
+                arr.append(line.split('|'))
 
-    for kohde in DBSession.query(Kohde).all():
-        data = random.choice(arr)
-        anonymize_entity(kohde, data)
-        kohde.slaskutus = "" """
+        for tilaaja in DBSession.query(Tilaaja).all():
+            data = random.choice(arr)
+            anonymize_entity(tilaaja, data)
+
+        for kohde in DBSession.query(Kohde).all():
+            data = random.choice(arr)
+            anonymize_entity(kohde, data)
+            kohde.slaskutus = ""
 
     return {}
